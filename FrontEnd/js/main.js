c9c5454f75903d8b52824242d85026c9678747a6
Williamscht <williamscht75@gmail.com>
2025-05-05 12:25:21 +0200

    Premier commit

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,11 +1,19 @@
-console.log("test");
-
-
 async function api() {
     const reponse = await fetch("http://localhost:5678/api/works");
     const works = await reponse.json();
     console.log(works);
+
+    for (let i = 0; i < works.length; i++) {
+        createProjectCard(works[i]);
+    }
+}
+
+
+function createFigure(data) {
+    const FigureCard = document.createElement("figure")
+    projetCard.innerHTML = `<img src=${data.imageUrl} alt=${data.title}><figcaption>${data.title}</figcaption>`;
+    document.querySelector(".gallery").appendChild(FigureCard);
+
 }
 
 api()
-
