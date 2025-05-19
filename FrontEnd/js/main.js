b43001ff36247b9fc2cbc80ce22145b31f0aa173
Williamscht <williamscht75@gmail.com>
2025-05-19 19:57:23 +0200

    Ajout des filtres pour afficher les travaux par catégorie

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,19 +1,99 @@
-async function api() {
-    const reponse = await fetch("http://localhost:5678/api/works");
-    const works = await reponse.json();
-    console.log(works);
+async function fetchWorks() {
+    try {
+        const response = await fetch("http://localhost:5678/api/works");
+        if (!response.ok) throw new Error(`Erreur HTTP : ${response.status}`);
+        const works = await response.json();
+        console.log(works);
 
-    for (let i = 0; i < works.length; i++) {
-        CreateFigure(works[i]);
+        for (let i = 0; i < works.length; i++) {
+            CreateFigure(works[i]);
+        }
+        return works;
+    }   catch (error) {
+        console.error(error);
     }
 }
 
 
 function CreateFigure(data) {
     const FigureCard = document.createElement("figure")
-    FigureCard.innerHTML = `<img src=${data.imageUrl} alt=${data.title}><figcaption>${data.title}</figcaption>`;
+    FigureCard.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}"><figcaption>${data.title}</figcaption>`;
     document.querySelector(".gallery").appendChild(FigureCard);
 
 }
 
-api()
+
+function generateGallery(works) {
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+    works.forEach(work => {
+      CreateFigure(work);
+    });
+  }
+
+
+
+async function fetchCategories() {
+    try {
+      const response = await fetch("http://localhost:5678/api/categories");
+  
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP : ${response.status}`);
+      }
+  
+      const categories = await response.json(); 
+      console.log(categories);
+      return categories;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  
+
+function generateFilterButtons(categories, works) {
+    const filterContainer = document.querySelector(".filters");
+    filterContainer.innerHTML = "";
+  
+   
+    const allButton = document.createElement("button");
+    allButton.innerText = "Tous";
+    allButton.addEventListener("click", () => {
+        generateGallery(works);
+    });
+    filterContainer.appendChild(allButton);
+  
+    
+    categories.forEach(category => {
+      const button = document.createElement("button");
+      button.innerText = category.name;
+  
+      button.addEventListener("click", () => {
+        const filteredWorks = works.filter(work => work.categoryId === category.id);
+        generateGallery(filteredWorks);
+      });
+  
+      filterContainer.appendChild(button);
+    });
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  async function init() {
+    const works = await fetchWorks();
+    const categories = await fetchCategories();
+    generateGallery(works);
+    generateFilterButtons(categories, works);
+  }
+
+  init ();
